25c402fbf087f58746b40a3e9993071cf2e6a0ee
是晓酥梨呀 <2060988267@qq.com>
2024-10-29 09:14:25 +0800

    完成

--- app/src/main/java/cn/xiaosuli/firstapp/BookRvItem.java
+++ app/src/main/java/cn/xiaosuli/firstapp/BookRvItem.java
@@ -4,27 +4,61 @@ import androidx.annotation.DrawableRes;
 
 public class BookRvItem {
     @DrawableRes
-    private Integer image;
-    private String text;
+    private Integer cover;
+    /* 书名 */
+    private String bookName;
+    /* 作者 */
+    private String author;
+    /* 出版社 */
+    private String publishingHouse;
+    /* 价格 */
+    private String price;
 
-    public BookRvItem(Integer image, String text) {
-        this.image = image;
-        this.text = text;
+    public BookRvItem(Integer cover, String bookName, String author, String publishingHouse, String price) {
+        this.cover = cover;
+        this.bookName = bookName;
+        this.author = author;
+        this.publishingHouse = publishingHouse;
+        this.price = price;
     }
 
-    public Integer getImage() {
-        return image;
+    public Integer getCover() {
+        return cover;
     }
 
-    public void setImage(Integer image) {
-        this.image = image;
+    public void setCover(Integer cover) {
+        this.cover = cover;
     }
 
-    public String getText() {
-        return text;
+    public String getBookName() {
+        return bookName;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishingHouse() {
+        return publishingHouse;
+    }
+
+    public void setPublishingHouse(String publishingHouse) {
+        this.publishingHouse = publishingHouse;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
     }
 }
